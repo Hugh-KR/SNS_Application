bc5593f7086a51732d0086ba8bbc924bfc516e13
창혁 <lee2317884@naver.com>
2022-06-02 10:36:17 +0900

    "데이터 모델링 완료"

--- app/src/main/java/com/capstone/sns/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/capstone/sns/navigation/AddPhotoActivity.kt
@@ -8,9 +8,11 @@ import android.os.Bundle
 import android.widget.Toast
 import com.capstone.sns.R
 import com.capstone.sns.navigation.model.ContentDTO
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_add_photo.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -64,6 +66,35 @@ class AddPhotoActivity : AppCompatActivity() {
 
         var storageRef = storage?.reference?.child("images")?.child(imageFileName)
 
+        //Promise method
+        storageRef?.putFile(photoUri!!)?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
+
+            //Insert downloadUrl of image
+            contentDTO.imageUrl = uri.toString()
+
+            //Insert uid of user
+            contentDTO.uid = auth?.currentUser?.uid
+
+            //Insert userId
+            contentDTO.userId = auth?.currentUser?.email
+
+            //Insert explain of content
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            //Insert timestamp
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
+
+        }
+        /**
         //Callback method
         storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
             storageRef.downloadUrl.addOnSuccessListener { uri ->
@@ -93,5 +124,6 @@ class AddPhotoActivity : AppCompatActivity() {
 
             }
         }
+        **/
     }
 }
