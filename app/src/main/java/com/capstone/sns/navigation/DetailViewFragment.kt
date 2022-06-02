98989f921849f0ceeff94cf81d368968e540889b
창혁 <lee2317884@naver.com>
2022-06-03 01:19:01 +0900

    "사진소개페이지 구현중"

--- app/src/main/java/com/capstone/sns/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/capstone/sns/navigation/DetailViewFragment.kt
@@ -5,11 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.capstone.sns.R
+import com.capstone.sns.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailViewFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
+        firestore = FirebaseFirestore.getInstance()
         return view
     }
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+        init{
+
+            firestore?.collection("images")?.addSnapshotListener {querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                contentUidList.clear()
+                for(snapshot in querySnapshot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                notifyDataSetChanged()
+            }
+
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+    }
+
 }
