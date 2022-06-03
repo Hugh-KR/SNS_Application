d9ab2fd824887bc5ee068e6d67ef0ad05dd4039f
창혁 <lee2317884@naver.com>
2022-06-03 23:42:19 +0900

    "사진페이지 어뎁터 진행중"

--- app/src/main/java/com/capstone/sns/navigation/UserFragment.kt
+++ app/src/main/java/com/capstone/sns/navigation/UserFragment.kt
@@ -6,10 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.capstone.sns.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UserFragment : Fragment(){
+    var fragmentView : View? = null
+    var firestore : FirebaseFirestore? = null
+    var uid : String? = null
+    var auth : FirebaseAuth? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
-        return view
+        var fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
+        return fragmentView
     }
+    inner class UserFragmentRecyclerViewAdapter :
 }
